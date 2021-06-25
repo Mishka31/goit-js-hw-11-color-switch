cad84cd748a5b24b7653a6609c92bace9e0d9fbf
Misha Krasnonos <muzishutka7@gmail.com>
2021-06-25 14:00:29 +0300

    start murkup

---
+++ index.js
@@ -0,0 +1,17 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+console.log(colors);
+
+const refs = {
+  body: document.querySelector("body"),
+  startBtn: document.querySelector("button[data-action-start]"),
+  stopBtn: document.querySelector("button[data-action-stop]"),
+};
+
+refs.body.style();
